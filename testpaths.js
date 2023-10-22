5dca76b75f7486418fa0e18c3374e5ba0b8bcb3e
bspurlin <bill@ideologue.net>
2023-10-22 15:29:29 +0000

    sort arrays of file names ignoring case

--- testpaths.js
+++ testpaths.js
@@ -8,13 +8,25 @@ fsobj=JSON.parse(fs.readFileSync("./fsobj.2"))
 
 function ff (fsobj,parent) {
     fsobj.paths = {};
+    fsobj.path = parent;
+//Sort the filenames case-insensitively
+    if (fsobj.files.length > 0) {
+	fsobj.files.sort((a,b) => {
+	    const nameA = a.filename.toUpperCase();
+	    const nameB = b.filename.toUpperCase();
+	    if (nameA < nameB) return -1;
+	    if (nameA > nameB)return 1;
+	    return 0
+	})
+    }		
+			
     for (let i =0; i < fsobj.directories.length; i++) {
 	let x = fsobj.directories[i];
-	fsobj.parent = parent + "." + i;
+	let parentt = parent + "." + i;
 	if(x) {
 	    let y = path.basename(x.dirname);
 	    fsobj.paths[y] = x ;
-	    ff(x,fsobj.parent)
+	    ff(x,parentt)
 	}
     }
 }
