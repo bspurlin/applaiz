d3696b9d0b4f6003a782e27969e8a798655baf5c
bill <bill@ideologue.net>
2023-11-05 09:36:43 -0500

    convert template.js into a module that can export eol-removed ejs templates

---
+++ template.js
@@ -0,0 +1,55 @@
+<table>
+  <tbody>
+    <tr class="indexhead">
+        <th class="indexcolicon"><img src="/icons/blank.gif"></th>
+        <th class="indexcolname" id="dirname">
+	    <%let aa=obj.dirname.split("/")%><%= aa[aa.length - 1]%>
+	</th>
+	<th>
+	</th>
+     </tr>
+    <tr class="indexbreakrow">
+        <th colspan="2"><hr></th>
+    </tr>
+    <tr >
+        <td >
+    	    <img class="dirselector" path=<%= obj.parent %> id="parent-a"  src="/icons/back.gif">
+        </td>
+        <td class="dirselector" path=<%= obj.parent %> id="parent">Parent Directory
+	</td>
+    </tr>
+
+<% for (let i = 0; i < obj.directories.length; i++) {%>
+    <tr >
+        <td class="indexcolicon"><img src="/icons/folder.gif">
+        </td>
+        <td id=dirname<%= obj.paths[obj.directories[i]] %> path=<%= obj.path + "." + obj.paths[obj.directories[i]]%> class="dirselector">
+    	    <%= obj.directories[i]%>
+        </td>
+        <td>
+        </td>
+    </tr>
+<%}%>
+
+<% for (let i = 0; i < obj.files.length; i++) {%>
+    <%let re = /.(m4a|mp3)$/;%>
+    <%let filename, name, artist, album;%>
+    <%filename = obj.files[i].filename;%>
+    <%if(obj.files[i].title) {
+        name = obj.files[i].title
+      } else {
+          name = filename.replace(re,"")
+      };%>
+    <%if(obj.files[i].album) album = obj.files[i].album;%>
+    <%if(obj.files[i].artist) artist = obj.files[i].artist;%>
+        <tr id="<%= i + 1000%>">
+	    <td class="indexcolicon" ><img class="soundfile" id="<%= i %>" src="/icons/sound2.gif" filename="<%- filename%>" artist="<%- artist%>" selectiontitle="<%- name%>" album="<%- album%>">
+	    </td>
+	    <td class="indexcolname"><%- name%>
+	    </td>
+	    <td>
+	    </td>
+	</tr>
+<%}%>
+  </tbody>
+</table>
