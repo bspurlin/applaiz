6eb22385e49873b6ce956bbebc8d7bd62ec397bf
bill <bill@ideologue.net>
2023-11-07 17:31:37 -0500

    added test for module to allow import by both ES6 and commonjs

--- template.js
+++ template.js
@@ -1,55 +1,46 @@
-<table>
+function mkTempl(i) {
+    return templates[i].replace(/(\r\n|\n|\r)/gm, "")
+}
+
+let templates = [`<table>
   <tbody>
-    <tr class="indexhead">
-        <th class="indexcolicon"><img src="/icons/blank.gif"></th>
-        <th class="indexcolname" id="dirname">
-	    <%let aa=obj.dirname.split("/")%><%= aa[aa.length - 1]%>
-	</th>
-	<th>
-	</th>
-     </tr>
-    <tr class="indexbreakrow">
-        <th colspan="2"><hr></th>
-    </tr>
-    <tr >
-        <td >
-    	    <img class="dirselector" path=<%= obj.parent %> id="parent-a"  src="/icons/back.gif">
-        </td>
-        <td class="dirselector" path=<%= obj.parent %> id="parent">Parent Directory
+    <tr class="indexhead"><th class="indexcolicon"><img class="dirselector" path=<%= obj.parent %> id="parent-a"  src="/icons/back.gif"></th><th class="indexcolname" id="dirname"><%let aa=obj.dirname.split("/")%><%= aa[aa.length - 1]%></th><th></th></tr>
+
+    <% for (let i = 0; i < obj.directories.length; i++) {%>
+   <tr >
+     <td class="indexcolicon"><img src="/icons/folder.gif">
+     </td>
+     <td id=dirname<%= obj.paths[obj.directories[i]] %> path=<%= obj.path + "." + obj.paths[obj.directories[i]]%> class="dirselector">
+       <%= obj.directories[i]%>
+     </td>
+     <td>
+     </td>
+   </tr>
+   <%}%>
+
+   <% for (let i = 0; i < obj.files.length; i++) {
+      let re = /.(m4a|mp3|wav|flac)$/;
+      let filename, name, artist, album, title;
+      filename = obj.files[i].filename;
+      name = filename.replace(re,"");
+      if(obj.files[i].title) {
+		title = obj.files[i].title;	       
+		if (title.length >= name.length ) name = obj.files[i].title
+	  };
+      if(obj.files[i].album) album = obj.files[i].album;
+      if(obj.files[i].artist) artist = obj.files[i].artist; %>
+      <tr id="<%= i + 1000%>">
+	<td class="indexcolicon" ><img class="soundfile" id="<%= i %>" src="/icons/sound2.gif" filename="<%- filename%>" artist="<%- artist%>" selectiontitle="<%- name%>" album="<%- album%>">
 	</td>
-    </tr>
-
-<% for (let i = 0; i < obj.directories.length; i++) {%>
-    <tr >
-        <td class="indexcolicon"><img src="/icons/folder.gif">
-        </td>
-        <td id=dirname<%= obj.paths[obj.directories[i]] %> path=<%= obj.path + "." + obj.paths[obj.directories[i]]%> class="dirselector">
-    	    <%= obj.directories[i]%>
-        </td>
-        <td>
-        </td>
-    </tr>
-<%}%>
-
-<% for (let i = 0; i < obj.files.length; i++) {%>
-    <%let re = /.(m4a|mp3)$/;%>
-    <%let filename, name, artist, album;%>
-    <%filename = obj.files[i].filename;%>
-    <%if(obj.files[i].title) {
-        name = obj.files[i].title
-      } else {
-          name = filename.replace(re,"")
-      };%>
-    <%if(obj.files[i].album) album = obj.files[i].album;%>
-    <%if(obj.files[i].artist) artist = obj.files[i].artist;%>
-        <tr id="<%= i + 1000%>">
-	    <td class="indexcolicon" ><img class="soundfile" id="<%= i %>" src="/icons/sound2.gif" filename="<%- filename%>" artist="<%- artist%>" selectiontitle="<%- name%>" album="<%- album%>">
-	    </td>
-	    <td class="indexcolname"><%- name%>
-	    </td>
-	    <td>
-	    </td>
-	</tr>
-<%}%>
+	<td class="indexcolname"><%- name%>
+	</td>
+	<td>
+	</td>
+      </tr>
+      <%}%>
   </tbody>
 </table>
+`
+	    ]
+
+if(typeof module === undefined) {} else { module.exports = { mkTempl } }
