693e574d0f6724f6832a6b1265ac65aa10f4a460
bill <bill@ideologue.net>
2026-01-14 18:12:12 -0500

    changes to modules and addend from tmp

--- addend.js
+++ addend.js
@@ -4,56 +4,59 @@ const fs = require('fs');
 const {ff, mkDirObj, countAttr,searchFsObj} = require("./modules.js");
 opt = require('node-getopt').create([
     ['i', 'input=ARG',  'input fsobj', undefined],
-    ['r' , ''                    , 'remove'],
+    ['r' , ''                    , 'replace'],
+    ['' ,'del='                   , 'delete'],  
     ['h' , 'help'                , 'display this help']
 ]).bindHelp().parseSystem();
+
+
+if((opt.options.del && opt.options.r) || opt.options.del == "-r") {
+    console.error("Cannot delete" + opt.options.del + " and replace at the same time\n")
+    return
+}
 
 if(opt.options.i){
     fsobj = JSON.parse(fs.readFileSync(opt.options.i));
 }
 else return;
 
-// 1st arg: The directory d in which dirobjs will be added, replaced, removed
 
-let d = [];
-if (opt.argv[0]) {
-    d = opt.argv[0]
-}
 
-let addendobj = {};
 let addendobj_aa = [];
 
-// 2nd arg: the dirobj file or files created by fsObj.js
+// 1st arg: the dirobj file or comma-delimited
+// list of files created by fsObj.js
 
-if (opt.argv[1]) {
-    let fsobj_files = opt.argv[1];
+if (opt.argv[0]) {
+    let fsobj_files = opt.argv[0];
     for (f of fsobj_files.split(",")) {
 	addendobj_aa.push(JSON.parse(fs.readFileSync(f)));
     }
+} else if (opt.options.del) {
+    const bdel=opt.options.del;
+    for (d of bdel.split(",")) {console.error("Here " + d);
+	addendobj_aa.push({"dirname": d})
+    }
 }
 
-if (process.env.APPLAIZ_DBG) console.error({argv: opt.argv, replace: opt.options.r,options: opt.options,d: d, addendobj: addendobj});
 
-
-if(d.length == 0){console.error("input full /-delimited path");process.exit(1)}
-
+if (process.env.APPLAIZ_DBG) console.error({argv: opt.argv, replace: opt.options.r,options: opt.options, addendobj_aa: addendobj_aa});
 
 
 let robj = ff(
     {lobj: fsobj,
      fMassage: (obj)=>{
-	 let r = -1;	 
-	 if (obj.dirname == d){
-	     console.error(d, "==", obj.dirname);
-	     for ( addendobj of addendobj_aa) {
-		 if (opt.options.r){
+	 let r = -1;	 	 
+	 for ( addendobj of addendobj_aa) {
+	     if (path.dirname(addendobj.dirname) == obj.dirname) {
+		 if (opt.options.r) {
 		     r = obj.directories.findIndex((x)=>x.dirname == addendobj.dirname);
 		     if (r >= 0){
 			 if (process.env.APPLAIZ_DBG) console.error(
 			     {
-				 "Replace ":r,
-				 "Old dirname": obj.dirname,
-				 "New dirname":addendobj.dirname
+				 "Replacing ":obj.directories[r].dirname,
+				 "Containing dirname": obj.dirname,
+				 "With dirname":addendobj.dirname
 			     }
 			 );
 			 if (obj.directories[r].perma) {
@@ -61,7 +64,7 @@ let robj = ff(
 			 } else {
 			     console.error(
 				 {
-				     "Replace ":r,
+				     "Replace - no perma ":r,
 				     "no obj.directories[r].perma":  obj.directories[r].dirname
 				 }
 			     );
@@ -69,7 +72,7 @@ let robj = ff(
 			 }
 			 obj.directories.splice(r,1,addendobj)
 		     }
-		 }else {
+		 } else if(opt.argv[0]){
 		     if (process.env.APPLAIZ_DBG) console.error(
 			 {
 			     "Add":  addendobj.dirname,
@@ -87,11 +90,19 @@ let robj = ff(
 		     )
 		 }
 	     }
+	 }     
+     },
+     fDir: (lobj,x)=>{
+	 for ( addendobj of addendobj_aa) {
+	     if (opt.options.del && addendobj.dirname == x.dirname){
+		 console.error({"Delete": x.dirname});
+		 lobj.directories = lobj.directories.filter(dir => dir.dirname != addendobj.dirname )
+	     }
 	 }
      }
-     
     }
 )
+    
 
 
 
